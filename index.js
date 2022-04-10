fa471ed7ef238a5d6b18d8b7d76d98fa02729be8
Gaddi <77565717+RYGJR@users.noreply.github.com>
2022-04-10 22:44:01 +0800

    added multiple graphs

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const uploadConfirm_heart = document.getElementById('uploadConfirm_heart').addEv
             }
                
             //put there the function to draw chart
-            console.log(value_heart);
+            
             const ctx = document.getElementById('heartChart').getContext('2d');
-            const myChart = new Chart(ctx, {
+            const heartChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: timeData_heart,
@@ -57,6 +57,62 @@ const uploadConfirm_heart = document.getElementById('uploadConfirm_heart').addEv
 });
 
 //Thermistor graph
+const timeData_thermistor = [];
+const value_thermistor = [];
+const valueName_thermistor = 'Temperature';
+const uploadConfirm_thermistor = document.getElementById('uploadConfirm_thermistor').addEventListener('click', () =>{
+    Papa.parse(document.getElementById('uploadfile_thermistor').files[0],
+    {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: function(results_thermistor){
+      
+            for(i = 0; i < results_thermistor.data.length; i++){
+                timeData_thermistor.push(results_thermistor.data[i].Timestamp);
+                value_thermistor.push(results_thermistor.data[i].Value);
+            }
+               
+            //put there the function to draw chart
+            console.log(value_thermistor);
+            const ctx1 = document.getElementById('thermistorChart').getContext('2d');
+            const thermistorChart = new Chart(ctx1, {
+            type: 'line',
+            data: {
+                labels: timeData_thermistor,
+                datasets: [{
+                    label: valueName_thermistor,
+                    data: value_thermistor,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)'
+                        
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)'
+                        // 'rgba(54, 162, 235, 1)',
+                        // 'rgba(255, 206, 86, 1)',
+                        // 'rgba(75, 192, 192, 1)',
+                        // 'rgba(153, 102, 255, 1)',
+                        // 'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: false
+                    }
+                }
+            }
+        });
+
+            
+        }
+    });
+});
 //Flex graph
 
 
