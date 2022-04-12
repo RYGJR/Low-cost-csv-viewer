d00c17fe48977a29936ef9ff0f8a200a9d910cf1
Gaddi <77565717+RYGJR@users.noreply.github.com>
2022-04-12 21:03:32 +0800

    added flex graph

--- index.js
+++ index.js
@@ -114,6 +114,62 @@ const uploadConfirm_thermistor = document.getElementById('uploadConfirm_thermist
     });
 });
 //Flex graph
+const timeData_flex = [];
+const value_flex = [];
+const valueName_flex = 'Flex Sensor';
+const uploadConfirm_flex = document.getElementById('uploadConfirm_flex').addEventListener('click', () =>{
+    Papa.parse(document.getElementById('uploadfile_flex').files[0],
+    {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        complete: function(results_flex){
+      
+            for(i = 0; i < results_flex.data.length; i++){
+                timeData_flex.push(results_flex.data[i].Timestamp);
+                value_flex.push(results_flex.data[i].Value);
+            }
+               
+            //put there the function to draw chart
+            console.log(value_flex);
+            const ctx2 = document.getElementById('flexChart').getContext('2d');
+            const flexChart = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                labels: timeData_flex,
+                datasets: [{
+                    label: valueName_flex,
+                    data: value_flex,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)'
+                        
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)'
+                        // 'rgba(54, 162, 235, 1)',
+                        // 'rgba(255, 206, 86, 1)',
+                        // 'rgba(75, 192, 192, 1)',
+                        // 'rgba(153, 102, 255, 1)',
+                        // 'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: false
+                    }
+                }
+            }
+        });
+
+            
+        }
+    });
+});
 
 
 
